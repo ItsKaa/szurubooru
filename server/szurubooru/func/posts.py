9ac9a287a92b3f8865eea36e7787c6017d8bbbea
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2024-11-13 00:08:03 +0100

    Fix regenerate thumbnails not liking the duplicate methods, revered the previous commit and moved the original code inside szuru-admin.

--- server/szurubooru/func/posts.py
+++ server/szurubooru/func/posts.py
@@ -700,25 +700,6 @@ def generate_post_thumbnail(post: model.Post) -> None:
     except errors.ProcessingError:
         files.save(get_post_thumbnail_path(post), EMPTY_PIXEL)
 
-
-def generate_post_thumbnail(post: model.Post) -> None:
-    assert post
-    if files.has(get_post_thumbnail_backup_path(post)):
-        content = files.get(get_post_thumbnail_backup_path(post))
-    else:
-        content = files.get(get_post_content_path(post))
-    try:
-        assert content
-        image = images.Image(content)
-        image.resize_fill(
-            int(config.config["thumbnails"]["post_width"]),
-            int(config.config["thumbnails"]["post_height"]),
-            keep_transparency=False,
-        )
-        files.save(get_post_thumbnail_path(post), image.to_jpeg())
-    except errors.ProcessingError:
-        files.save(get_post_thumbnail_path(post), EMPTY_PIXEL)
-
 def update_post_tags(
     post: model.Post, tag_names: List[str]
 ) -> List[model.Tag]:
